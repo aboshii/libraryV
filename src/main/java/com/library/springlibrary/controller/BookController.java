1ee1fa7c420581f08222cfafe97ac92b4aa0c18e
Michał Zgiet <4boshii@gmail.com>
2023-05-04 17:22:10 +0200

    added thymyleaf to pom project, added few html files and GET/POST controllers

---
+++ src/main/java/com/library/springlibrary/controller/BookController.java
@@ -0,0 +1,13 @@
+package com.library.springlibrary.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AddNewBookController {
+
+    @RequestMapping("/addbook")
+    void addNewBook(){
+
+    }
+}
